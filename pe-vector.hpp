8b3415b73e3b07f5198167edeeb0735f999a7b80
makschadin <maxchadin080418@gmail.com>
2026-03-25 15:02:41 +0300

    Update pe-vector.hpp

--- pe-vector.hpp
+++ pe-vector.hpp
@@ -1,5 +1,7 @@
 #ifndef PE_VECTOR_HPP
 #define PE_VECTOR_HPP
+#include <cstdef>
+
 namespace knk {
   template< class T >
   class Vector {
@@ -11,6 +13,15 @@ namespace knk {
     T* data;
     size_t size, capacity;
   };
+}
+template< class T >
+knk::Vector< T >::Vector():
+  data(nullptr),
+  size_(0),
+  capacity_(0)
+{}
+knk::Vector< T >::~Vector(){
+  delete[] data_;
 }
 
 #endif

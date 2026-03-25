a1fc262823312863cfbeb8da4b74658bc728aaff
makschadin <maxchadin080418@gmail.com>
2026-03-25 17:00:57 +0300

    Update pe-vector.hpp

--- pe-vector.hpp
+++ pe-vector.hpp
@@ -10,19 +10,22 @@ namespace knk {
     Vector();
     Vector(const Vector< T >& rhs) = delete;
     Vector(size_t size, const T& value);
-    explicit Vector(size_t size);
 
     Vector< T >& operator=(const Vector< T >& rhs) = delete;
 
     bool isEmpty() const noexcept;
     size_t getSize() const noexcept;
 
+    //Реализовать и протестировать
+    size_t getCapacity() const noexcept;
     void pushBack(const T&);
     void popBack();
 
     private:
     T* data;
     size_t size_, capacity_;
+
+    explicit Vector(size_t size);
   };
 }
 
